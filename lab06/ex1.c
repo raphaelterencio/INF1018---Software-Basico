acd47b11a6d9ca93da54b77f79781c48c71ed6c1
raphaelterencio <125285542+raphaelterencio@users.noreply.github.com>
2026-09-24 15:15:33 +0000

    adiciona relatórios e implementações de código para o laboratório 06

---
+++ lab06/ex1.c
@@ -0,0 +1,19 @@
+
+int nums[] = {10, -21, -30, 45};
+int main() {
+  int i, *p;
+  for (i = 0, p = nums; i != 4; i++, p++)
+    printf("%d\n", *p);
+  return 0;
+}
+
+
+int nums[] = {10, -21, -30, 45};
+int main() {
+  int i, *p;
+  int sum = 0;
+  for (i = 0, p = nums; i != 4; i++, p++)
+    sum += *p;
+  printf("%d\n", sum);
+  return 0;
+}
